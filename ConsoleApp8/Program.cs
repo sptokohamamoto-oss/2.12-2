34b7033f1cf8e232e68c16c20f73d8e506ff7682
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-09 18:26:32 +0900

    a

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        string[] fruits = { "りんご", "みかん", "ぶどう" };
+
+        foreach (var item in fruits) 
+        {
+        
+        Console.WriteLine(item);
+        
+        }
+    
+   
+    
+    
+    }
+
+
+
+}
